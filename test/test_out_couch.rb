7006ca4c554c78056efd34dd8bf64891db780891
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2017-01-13 18:24:24 +0900

    test: Add initial test cases

---
+++ test/test_out_couch.rb
@@ -0,0 +1,62 @@
+require "fluent/test"
+require "fluent/plugin/out_couch"
+
+class CouchOutputTest < Test::Unit::TestCase
+  def setup
+    Fluent::Test.setup
+  end
+  
+  CONFIG = %[
+    type couch
+    database fluent
+  ]
+
+  CONFIG_UPDATE_DOC = %[
+    type couch
+    database fluent
+    doc_key_field key
+    update_docs true
+  ]
+
+  def create_driver(config = CONFIG)
+    Fluent::Test::BufferedOutputTestDriver.new(Fluent::CouchOutput).configure(config)
+  end
+
+  def test_configure
+    d = create_driver
+    assert_equal('fluent', d.instance.database)
+    assert_equal('localhost', d.instance.host)
+    assert_equal('5984', d.instance.port)
+    assert_equal('http', d.instance.protocol)
+    assert_nil(d.instance.refresh_view_index)
+    assert_nil(d.instance.user)
+    assert_nil(d.instance.password)
+    assert_false(d.instance.instance_variable_get(:@update_docs))
+    assert_nil(d.instance.doc_key_field)
+    assert_nil(d.instance.doc_key_jsonpath)
+  end
+
+  def test_write
+    d = create_driver
+    previous_rows = d.instance.instance_variable_get(:@db).all_docs["total_rows"]
+    time = Time.now.to_i
+    d.emit({"message" => "record"}, time)
+    d.run
+    rows = d.instance.instance_variable_get(:@db).all_docs["total_rows"]
+    assert_equal(rows, previous_rows + 1)
+  end
+
+  def test_write_update_doc
+    d = create_driver(CONFIG_UPDATE_DOC)
+    time = Time.now.to_i
+    d.emit({"key" => "record-1", "message" => "record"}, time)
+    d.run
+    previous_rows = d.instance.instance_variable_get(:@db).all_docs["total_rows"]
+    d.emit({"key" => "record-1", "message" => "record-mod"}, time)
+    d.run
+    rows = d.instance.instance_variable_get(:@db).all_docs["total_rows"]
+    record = d.instance.instance_variable_get(:@db).get("record-1")
+    assert_equal(rows, previous_rows)
+    assert_equal("record-mod", record["message"])
+  end
+end
