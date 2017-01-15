bf5049ed424ca515d866e4dc59cefe775b5b0e3e
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2017-01-15 15:32:10 +0900

    Prepare database in test cases

--- test/test_out_couch.rb
+++ test/test_out_couch.rb
@@ -5,25 +5,35 @@ class CouchOutputTest < Test::Unit::TestCase
   def setup
     Fluent::Test.setup
   end
-  
+
+  DATABASE_NAME = "fluent_test"
+  COUCHHOST = ENV['COUCHHOST'] || "http://127.0.0.1:5984"
+
   CONFIG = %[
     type couch
-    database fluent
+    database #{DATABASE_NAME}
   ]
 
   CONFIG_UPDATE_DOC = %[
     type couch
-    database fluent
+    database #{DATABASE_NAME}
     doc_key_field key
     update_docs true
   ]
 
   CONFIG_JSONPATH = %[
     type couch
-    database fluent
+    database #{DATABASE_NAME}
     doc_key_field key
     doc_key_jsonpath $.nested.key
   ]
+
+  def prepare_db
+    @cr = CouchRest.new(COUCHHOST)
+    @db = @cr.database(DATABASE_NAME)
+    @db.delete! rescue nil
+    @cr.create_db(DATABASE_NAME)
+  end
 
   def create_driver(config = CONFIG)
     Fluent::Test::BufferedOutputTestDriver.new(Fluent::CouchOutput).configure(config)
@@ -31,7 +41,7 @@ class CouchOutputTest < Test::Unit::TestCase
 
   def test_configure
     d = create_driver
-    assert_equal('fluent', d.instance.database)
+    assert_equal(DATABASE_NAME, d.instance.database)
     assert_equal('localhost', d.instance.host)
     assert_equal('5984', d.instance.port)
     assert_equal('http', d.instance.protocol)
@@ -43,36 +53,66 @@ class CouchOutputTest < Test::Unit::TestCase
     assert_nil(d.instance.doc_key_jsonpath)
   end
 
-  def test_write
-    d = create_driver
-    previous_rows = d.instance.db.all_docs["total_rows"]
-    time = Time.now.to_i
-    d.emit({"message" => "record"}, time)
-    d.run
-    rows = d.instance.db.all_docs["total_rows"]
-    assert_equal(rows, previous_rows + 1)
+  class WriteTest < self
+    def setup
+      @d = create_driver
+      prepare_db
+    end
+
+    def teardown
+      @db.delete!
+    end
+
+    def test_write
+      previous_rows = @d.instance.db.all_docs["total_rows"]
+      time = Time.now.to_i
+      @d.emit({"message" => "record"}, time)
+      @d.run
+      rows = @d.instance.db.all_docs["total_rows"]
+      assert_equal(rows, previous_rows + 1)
+    end
   end
 
-  def test_write_update_doc
-    d = create_driver(CONFIG_UPDATE_DOC)
-    time = Time.now.to_i
-    d.emit({"key" => "record-1", "message" => "record"}, time)
-    d.run
-    previous_rows = d.instance.db.all_docs["total_rows"]
-    d.emit({"key" => "record-1", "message" => "record-mod"}, time)
-    d.run
-    rows = d.instance.db.all_docs["total_rows"]
-    record = d.instance.db.get("record-1")
-    assert_equal(rows, previous_rows)
-    assert_equal("record-mod", record["message"])
+  class WriteWithUpdateDocTest < self
+    def setup
+      @d = create_driver(CONFIG_UPDATE_DOC)
+      prepare_db
+    end
+
+    def teardown
+      @db.delete!
+    end
+
+    def test_write_update_doc
+      time = Time.now.to_i
+      @d.emit({"key" => "record-1", "message" => "record"}, time)
+      @d.run
+      previous_rows = @d.instance.db.all_docs["total_rows"]
+      @d.emit({"key" => "record-1", "message" => "record-mod"}, time)
+      @d.run
+      rows = @d.instance.db.all_docs["total_rows"]
+      record = @d.instance.db.get("record-1")
+      assert_equal(rows, previous_rows)
+      assert_equal("record-mod", record["message"])
+    end
   end
 
-  def test_write_doc_key_jsonoath
-    d = create_driver(CONFIG_JSONPATH)
-    time = Time.now.to_i
-    d.emit({"nested" => {"key" => "record-nested", "message" => "record"}}, time)
-    d.run
-    record = d.instance.db.get("record-nested")
-    assert_equal({"key" => "record-nested", "message" => "record"}, record["nested"])
+  class WriteWithJsonpathTest < self
+    def setup
+      @d = create_driver(CONFIG_JSONPATH)
+      prepare_db
+    end
+
+    def teardown
+      @db.delete!
+    end
+
+    def test_write_doc_key_jsonpath
+      time = Time.now.to_i
+      @d.emit({"nested" => {"key" => "record-nested", "message" => "record"}}, time)
+      @d.run
+      record = @d.instance.db.get("record-nested")
+      assert_equal({"key" => "record-nested", "message" => "record"}, record["nested"])
+    end
   end
 end
